92435de7593e70d488297a461a129e1d758222c8
mfrachet <marvin.frachet@zenika.com>
2018-05-31 17:08:40 +0200

    feat(alphabetical): statelessness

--- packages/i18n-lint/src/verifiers/jsonVerifier.js
+++ packages/i18n-lint/src/verifiers/jsonVerifier.js
@@ -1,17 +1,25 @@
+const alphabeticRule = (state, currentKey, currentIndex, jsonEntry, keys) => {
+  const nextKey = keys[currentIndex + 1];
+
+  if (nextKey && nextKey < currentKey) {
+    return [...state, { error: true, message: `current key ${currentKey} is bigger than its next key ${nextKey}` }];
+  }
+
+  return state;
+};
+
+const rules = [alphabeticRule];
+
 export const validateJson = jsonEntries => {
   const keys = Object.keys(jsonEntries);
 
   return keys.reduce((state, currentKey, index) => {
-    const nextKey = keys[index + 1];
-
-    if (nextKey && nextKey < currentKey) {
-      state = [...state, { error: true, message: `current key ${currentKey} is bigger than its next key ${nextKey}` }];
-    }
+    const nextState = rules.reduce((acc, rule) => [...acc, ...rule(state, currentKey, index, jsonEntries, keys)], []);
 
     if (typeof jsonEntries[currentKey] === 'object') {
-      state = state.concat(validateJson(jsonEntries[currentKey]));
+      return [...nextState, ...validateJson(jsonEntries[currentKey])];
     }
 
-    return state;
+    return nextState;
   }, []);
 };
