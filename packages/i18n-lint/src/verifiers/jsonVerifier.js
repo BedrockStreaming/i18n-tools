1b88879663cfc0bbc0177985d1afc34692a4eebe
mfrachet <marvin.frachet@zenika.com>
2018-05-31 16:54:31 +0200

    wip

--- packages/i18n-lint/src/verifiers/jsonVerifier.js
+++ packages/i18n-lint/src/verifiers/jsonVerifier.js
@@ -1,19 +1,17 @@
 export const validateJson = jsonEntries => {
-  let reports = [];
-
   const keys = Object.keys(jsonEntries);
 
-  keys.forEach((currentKey, index) => {
+  return keys.reduce((state, currentKey, index) => {
     const nextKey = keys[index + 1];
 
     if (nextKey && nextKey < currentKey) {
-      reports.push({ error: true, message: `current key ${currentKey} is bigger than its next key ${nextKey}` });
+      state = [...state, { error: true, message: `current key ${currentKey} is bigger than its next key ${nextKey}` }];
     }
 
     if (typeof jsonEntries[currentKey] === 'object') {
-      reports = reports.concat(validateJson(jsonEntries[currentKey]));
+      state = state.concat(validateJson(jsonEntries[currentKey]));
     }
-  });
 
-  return reports;
+    return state;
+  }, []);
 };
