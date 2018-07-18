d104394be3dba7d376441ea1a35760585d370c13
Florent Lepretre <florent.lepretre@gmail.com>
2018-07-18 10:38:22 +0200

    chore(build): setup build for react-i18n

--- packages/react-i18n/es/index.js
+++
@@ -1,5 +0,0 @@
-export { translate as translateFunction, buildList as buildListFunction } from './utils/i18n.utils';
-
-export { translate } from './components/i18n.container';
-export { Trans } from './components/i18n.component';
-export { I18nProvider } from './components/i18n.provider';
