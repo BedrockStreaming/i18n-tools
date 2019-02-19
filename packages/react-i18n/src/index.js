c9783becf6a5a33094e045c568903529ad54a338
Florent Lepretre <florent.lepretre@gmail.com>
2019-02-19 08:01:15 +0100

    fix(react-i18n): exports hook in index.js

--- packages/react-i18n/src/index.js
+++ packages/react-i18n/src/index.js
@@ -4,3 +4,4 @@ export { translate } from './components/i18n.container';
 export { Trans } from './components/i18nString.component';
 export { HtmlTrans } from './components/i18nElement.component';
 export { I18nProvider } from './components/i18n.provider';
+export { useTranslate } from './hooks/useTranslate';
