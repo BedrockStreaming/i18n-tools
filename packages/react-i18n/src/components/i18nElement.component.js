670dfae8e8595057ecaea74a1ce7f84b805213bd
Geoffrey <geoffreycdias@gmail.com>
2019-11-25 15:49:06 +0100

    add renderers to <Trans />

--- packages/react-i18n/src/components/i18nElement.component.js
+++ packages/react-i18n/src/components/i18nElement.component.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Context } from '../context/i18n.context';
 
-export const HtmlTrans = ({ i18nKey, data, number, general, element: Element, ...props }) => (
+export const HtmlTrans = ({ i18nKey, data, number, general, element: Element, renderers, ...props }) => (
   <Context.Consumer>
-    {t => <Element {...props} dangerouslySetInnerHTML={{ __html: t(i18nKey, data, number, general) }} />}
+    {t => <Element {...props} dangerouslySetInnerHTML={{ __html: t(i18nKey, data, number, general, renderers) }} />}
   </Context.Consumer>
 );
 
@@ -20,4 +20,5 @@ HtmlTrans.propTypes = {
   data: PropTypes.object,
   number: PropTypes.number,
   general: PropTypes.bool,
+  renderers: PropTypes.object,
 };
