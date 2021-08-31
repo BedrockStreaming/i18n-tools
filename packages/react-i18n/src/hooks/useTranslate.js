8920de7981ba30ca4ed9bbf37e1a10a32307451b
Florent Lepretre <florent.lepretre@gmail.com>
2021-08-31 09:38:10 +0200

    wip

--- packages/react-i18n/src/hooks/useTranslate.js
+++ packages/react-i18n/src/hooks/useTranslate.js
@@ -1,4 +1,9 @@
 import { useContext } from 'react';
 import { Context } from '../context/i18n.context';
 
-export const useTranslate = () => useContext(Context);
+const emptyObject = {};
+export const useTranslate = () => {
+  const t = useContext(Context);
+
+  return (key, data = emptyObject) => t(key, data);
+};
